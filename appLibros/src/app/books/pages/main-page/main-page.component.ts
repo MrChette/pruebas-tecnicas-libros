7a2a27276e248cc146a04faf3ba7d4e0364a99ed
Chette <80092129+MrChette@users.noreply.github.com>
2023-09-06 04:10:10 +0200

    [NEW] Lista de lectura

--- appLibros/src/app/books/pages/main-page/main-page.component.ts
+++ appLibros/src/app/books/pages/main-page/main-page.component.ts
@@ -9,10 +9,9 @@ import { BooksServiceService } from '../../services/books-service.service';
 })
 export class MainPageComponent implements OnInit {
 
-  public books: Book[] = [];
-  public booksList: Book[] = [];
-  public genres: string[] = [];
-  public nBooks: number = 0;
+  private books: Book[] = [];
+  private booksList: Book[] = [];
+  private genres: string[] = [];
 
   constructor(
       private booksService : BooksServiceService){}
@@ -21,17 +20,24 @@ export class MainPageComponent implements OnInit {
     this.booksService.getBooks()
       .subscribe(books => {
         this.books = books;
-        this.getNumberBooks();
-        this.getGenres(); // Llama a getGenres() después de cargar los libros
+        this.getGenres;
       });
   }
 
-  getGenres() {
-    this.genres = [...new Set(this.books.map((item) => item.book.genre))];
+  public get getGenres() {
+    return this.genres = [...new Set(this.books.map((item) => item.book.genre))];
   }
 
-  getNumberBooks(): void{
-    this.nBooks = this.books.length;
+  public get getBooks() {
+    return this.books;
+  }
+
+  public get getBookList(): Book[] {
+    return this.booksList;
+  }
+
+  public get nBooks(): number {
+    return this.books.length;
   }
 
 
@@ -40,6 +46,14 @@ export class MainPageComponent implements OnInit {
     if (index !== -1) {
       this.books.splice(index, 1); // Elimina el libro de books
       this.booksList.push(book);  // Agrega el libro a booksList
+    }
+  }
+
+  addToBooks( book: Book): void {
+    const index = this.booksList.indexOf(book);
+    if (index !== -1) {
+      this.booksList.splice(index, 1); // Elimina el libro de booksList
+      this.books.push(book);  // Agrega el libro a books
     }
   }
 
